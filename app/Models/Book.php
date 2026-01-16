0806c34875317c97aae7ad8487d6069217722308
Богдан <overgoousez1@gmail.com>
2026-01-16 18:26:29 +0500

    create models with relationships and softdelete in books

---
+++ app/Models/Book.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'author',
+        'publishing',
+        'book_text',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'login',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    public function books(){
+        return $this->hasMany(Book::class);
+    }
+
+    public function allowedGuests(){
+        return $this->belongsToMany(User::class, 'user_accesses', 'owner_id', 'guest_id');
+    }
+
+    public function accessibleLibraries(){
+        return $this->belongsToMany(User::class, 'user_accesses', 'guest_id', 'owner_id');
+    }
+}
